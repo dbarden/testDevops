f69c61f6ce2a0b08238000b74c5671b6505ed317
Daniel Barden <daniel.barden@mhp.com>
2020-07-10 21:54:14 +0200

    Fix SwiftLint issues

--- testAzureDevOpsTests/TestAzureDevOpsTests.swift
+++ testAzureDevOpsTests/TestAzureDevOpsTests.swift
@@ -9,7 +9,7 @@
 import XCTest
 @testable import testAzureDevOps
 
-class testAzureDevOpsTests: XCTestCase {
+class TestAzureDevOpsTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
